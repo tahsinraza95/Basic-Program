54ec43d62dd8d0a0c63d04ecdd7a44829bf1cf5b
tahsinraza95 <tahsinr693@gmail.com>
2023-01-30 11:32:11 +0530

    Employee Wages

---
+++ Employee Wages/Employee Wages/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Employee_Wages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int RANDOM = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == RANDOM)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
